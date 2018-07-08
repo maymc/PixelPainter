7d6ccdaec36de3c5a79ba66c24906e219cff2f52
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-07 20:06:27 -1000

    removed the extra css link in the html file that I accidentally added. For the js file, I was able to get the colors onto the color swatch

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,10 @@
 let pixelPainter = (function(){
 
+    let colors = ["indianred", "lightcoral","salmon","pink","lightpink","hotpink","coral","tomato","orangered","gold","yellow","lightyellow","lavender","thistle","plum","greenyellow","chartreuse","lawngreen","aqua","cyan","lightcyan","cornsilk","blanchedalmond","bisque","white","snow","honeydew","gainsboro","lightgray","silver","darksalmon","lightsalmon","crimson","deeppink","mediumvioletred","palevioletred","darkorange","orange","lemonchiffon","lightgoldenrodyellow","papayawhip","violet","orchid","fuchsia","lime","limegreen","palegreen","paleturquoise","aquamarine","turquoise","navajowhite","wheat","burlywood","dimgray","lightslategray","dimslategray","skyblue","lightskyblue","dodgerblue","purple","darkorchid","indigo","seagreen","forestgreen","green","teal","darkcyan"];
+
+    console.log(colors[0]);
+
+    console.log(colors.length);
 
     const pixelPainterDiv = document.getElementById("pixelPainter");
 
@@ -13,16 +18,21 @@ let pixelPainter = (function(){
         colorSwatchBody.appendChild(rowElem);
 
         for(var j=1; j<=6; j++){
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
-            rowElem.appendChild(pixel);
+            let pixelElem = document.createElement("div");
+            pixelElem.className = "pixel";
+            rowElem.appendChild(pixelElem);
         }
-
     }
 
-    let colors = ["indianred", "lightcoral","salmon","pink","lightpink","hotpink","coral","tomato","orangered","gold","yellow","lightyellow","lavender","thistle","plum","greenyellow","chartreuse","lawngreen","aqua","cyan","lightcyan","cornsilk","blanchedalmond","bisque","white","snow","honeydew","gainsboro","lightgray","silver","darksalmon","lightsalmon","crimson","deeppink","mediumvioletred","palevioletred","darkorange","orange","lemonchiffon","lightgoldenrodyellow","papayawhip","violet","orchid","fuchsia","lime","limegreen","palegreen","paleturquoise","aquamarine","turquoise","navajowhite","wheat","burlywood","dimgray","lightslategray","dimslategray","skyblue","lightskyblue","dodgerblue","purple","darkorchid","indigo","seagreen","forestgreen","green","teal","darkcyan"];
-
     console.log(colors.length);
+    console.log("test: " + colors);
+
+    var pixelColor = document.getElementsByClassName("pixel");
+    for(var i=0; i<colors.length; i++){
+        pixelColor[i].style.backgroundColor = colors[i];
+    }
+
+
     
 
 

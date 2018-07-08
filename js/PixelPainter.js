4d10da70ea78f9f75789650e73028d902a1d4d1d
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-08 10:55:41 -1000

    Created the canvas grid. Added event listeners to each square. Created a working function that colors each square with the color chosen from the palette. The function is successful in retaining the color to color other squares until another color is chosen.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,8 +2,10 @@ let pixelPainter = (function(){
 
     let colors = ["indianred","palegoldenrod","darkred","turquoise","thistle","lawngreen","lightcoral","indigo","salmon", "darksalmon","paleturquoise","mediumturquoise","lightslategray","lavender","dodgerblue","lightsalmon","pink","slategray","firebrick","darkmagenta","aquamarine","cornflowerblue","mediumslateblue","lightpink","lime","deepskyblue","peachpuff","hotpink","deeppink","khaki","crimson","limegreen","orange","midnightblue","darkslategray","plum","darkturquoise","purple","chartreuse","mediumvioletred","darkorange","cadetblue","slateblue","violet","mediumblue","burlywood","red","tan","steelblue","royalblue","rosybrown","sandybrown","orchid","lightsteelblue","goldenrod","rebeccapurple","aqua","cyan","chocolate","saddlebrown","greenyellow","sienna","brown","maroon","darkseagreen","darkorchid"];
 
+    let selectedColor = "";
+
     //Check that there are enough colors to fill the colorswatch palette
-    console.log(colors.length);
+    console.log("num colors in palette: " + colors.length);
 
     const pixelPainterDiv = document.getElementById("pixelPainter");
 
@@ -24,7 +26,7 @@ let pixelPainter = (function(){
         }
     }
 
-    //apply a color to each pixel
+    //apply a color to each pixel and add an event listener to each pixel
     var pixelColor = document.getElementsByClassName("pixel");
     for(var i=0; i<colors.length; i++){
         pixelColor[i].style.backgroundColor = colors[i];
@@ -32,8 +34,36 @@ let pixelPainter = (function(){
     }
 
     function pickColor(){
-        //Check that the eventlistener click works
-        console.log(this.style.backgroundColor);
+        //Store the color temporarily in a variable. It will be used to color the canvas
+        selectedColor = this.style.backgroundColor;
+        console.log("selectedColor: " + selectedColor);
+    }
+
+    //Create the canvas grid
+    let canvasBody = document.createElement("div");
+    canvasBody.id = "canvasBody";
+    pixelPainterDiv.appendChild(canvasBody);
+
+    for(var i=1; i<=10; i++){
+        let canvasRowElem = document.createElement("div");
+        canvasRowElem.className = "canvasRow";
+        canvasBody.appendChild(canvasRowElem);
+
+        for(var j=1; j<=10; j++){
+            let squareElem = document.createElement("div");
+            squareElem.className = "square";
+            canvasRowElem.appendChild(squareElem);
+        }
+    }
+
+    var gridSquare = document.getElementsByClassName("square");
+    for(var i=0; i<gridSquare.length; i++){
+        gridSquare[i].addEventListener("click", colorSquare);
+    }
+
+    function colorSquare(){
+        console.log("check selectedColor: " + selectedColor);
+        this.style.backgroundColor = selectedColor;
     }
 
 
